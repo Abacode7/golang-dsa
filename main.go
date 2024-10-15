d09dbc18dc47fcce8314fd121cb48e9afe5ba11e
Abraham O <olamipoabraham@gmail.com>
2024-10-15 07:11:48 +0100

    Add initial commit for Golang dsa

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"dsa/fibonacci"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Data Structures and Algorithm in Golang")
+
+	fmt.Println("The fibonnaci of 7 is: ", fibonacci.FibN(7))
+}
