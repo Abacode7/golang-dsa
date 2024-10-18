88f7e59c36ad0b556363bf3f432d09be5628cb22
Abraham O <olamipoabraham@gmail.com>
2024-10-18 09:29:15 +0100

    Add complexity to the stack algorithm

--- stack/stack.go
+++ stack/stack.go
@@ -11,10 +11,17 @@ func NewStack() *Stack {
 
 /** Todo: push, pop, peek*/
 
+// Push adds value to the (conceptual) head of the stack
+// O(1) time, O(1) extra space
+// NB: The append operation extra time O(N) when the underlying array allocation
+// is full and moves value to a one with a bigger capacity
 func (r *Stack) Push(item interface{}) {
 	r.data = append(r.data, item)
 }
 
+// Pop removes value from the (conceptual) head of the stack
+// O(1) time, O(1) extra space
+// NB: The extract operator (:) still references the same underlying array
 func (r *Stack) Pop() interface{} {
 	if len(r.data) == 0 {
 		return nil
@@ -25,6 +32,8 @@ func (r *Stack) Pop() interface{} {
 	return lastItem
 }
 
+// Peek views value from the (conceptual) head of the stack
+// O(1) time, O(1) extra space
 func (r *Stack) Peek() interface{} {
 	if len(r.data) == 0 {
 		return nil
